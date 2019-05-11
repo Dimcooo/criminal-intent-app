112a8528dff8df90ddbc924471310929684bd05d
Dzmitry Yakubovich <DYakubovich@ibagroup.eu>
2019-05-11 16:51:27 +0300

    Added fragment to activity

--- CriminalIntent/app/src/main/java/com/dimco/criminalintent/activity/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/dimco/criminalintent/activity/CrimeActivity.java
@@ -1,10 +1,13 @@
 package com.dimco.criminalintent.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.dimco.criminalintent.R;
+import com.dimco.criminalintent.fragment.CrimeFragment;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -12,5 +15,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentConrainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentConrainer, fragment)
+                    .commit();
+        }
     }
 }
--- CriminalIntent/app/src/main/java/com/dimco/criminalintent/fragment/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/dimco/criminalintent/fragment/CrimeFragment.java
@@ -1,9 +1,17 @@
 package com.dimco.criminalintent.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.dimco.criminalintent.R;
 import com.dimco.criminalintent.entity.Crime;
 
 public class CrimeFragment extends Fragment {
@@ -12,5 +20,31 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        EditText titleField = v.findViewById(R.id.crime_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                crime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        return v;
     }
 }
